712ee138144304e1b823328bf767bb889428f7b3
Leonardo Thome Pires <leonardo@weedseeker.net>
2017-02-12 16:32:39 -0200

    Reverting to empty repo

--- client/app/account/settings/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-const angular = require('angular');
-import SettingsController from './settings.controller';
-
-export default angular.module('mathPlaygroundApp.settings', [])
-  .controller('SettingsController', SettingsController)
-  .name;
