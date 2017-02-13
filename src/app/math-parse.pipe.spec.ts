892b299bbf5de6d5a78361d2226d181047d5ec92
Leonardo Thome Pires <leonardo@weedseeker.net>
2017-02-13 07:16:28 -0200

    Started adding unit tests

--- src/app/math-parse.pipe.spec.ts
+++ src/app/math-parse.pipe.spec.ts
@@ -2,10 +2,17 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { MathParsePipe } from './math-parse.pipe';
+const mathjs = require('mathjs');
+const MathNode = mathjs.MathNode;
 
 describe('MathParsePipe', () => {
   it('create an instance', () => {
     const pipe = new MathParsePipe();
     expect(pipe).toBeTruthy();
+  });
+  it('returns MathExpression', () => {
+    const pipe = new MathParsePipe();
+    const result = pipe.transform('x^2');
+    expect(result instanceof MathNode).toBeTruthy();
   });
 });
